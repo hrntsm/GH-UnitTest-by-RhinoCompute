2e8f27327ff9c68e7f7383e1a20c12dd4369c398
hrntsm <contact@hrntsm.com>
2021-04-17 23:25:57 +0900

    init

---
+++ GHCITest/GHCITestInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace GHCITest
+{
+    public class GHCITestInfo : GH_AssemblyInfo
+    {
+        public override string Name => "GHCITest Info";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("D4183AF1-684A-4196-8B62-A0311468622A");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
---
+++ GHCITestTests/GHCITestComponentTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Compute;
+using Newtonsoft.Json;
+
+namespace GHCITest.Tests
+{
+    [TestClass]
+    public class GHCITestComponentTests
+    {
+        [TestMethod]
+        public void GHCITestComponentTest()
+        {
+            ComputeServer.WebAddress = "http://localhost:8081/";
+
+            var definitionName = "testgh.gh";
+            var definitionPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            definitionPath = Path.GetDirectoryName(definitionPath);
+            definitionPath = Path.Combine(definitionPath, definitionName);
+
+            var trees = new List<GrasshopperDataTree>();
+
+            var value1 = new GrasshopperObject(10);
+            var param1 = new GrasshopperDataTree("A");
+            param1.Add("0", new List<GrasshopperObject> { value1 });
+            trees.Add(param1);
+
+            var value2 = new GrasshopperObject(35);
+            var param2 = new GrasshopperDataTree("B");
+            param2.Add("0", new List<GrasshopperObject> { value2 });
+            trees.Add(param2);
+
+
+            var result = Rhino.Compute.GrasshopperCompute.EvaluateDefinition(definitionPath, trees);
+            var data = result[0].InnerTree.First().Value[0].Data;
+            var parsed = JsonConvert.DeserializeObject<Dictionary<string, string>>(data);
+            var obj = Rhino.FileIO.File3dmObject.FromJSON(parsed);
+
+            Console.WriteLine("Result: {0}", obj.GetType());
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
